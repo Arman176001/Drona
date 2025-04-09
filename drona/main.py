4129e0dbd5f92d4c28865ec19f989eec498e21ec
Arman176001 <armanchaudhary176001@gmail.com>
2025-04-09 13:05:30 +0530

    Update main.py

--- drona/main.py
+++ drona/main.py
@@ -38,7 +38,7 @@ app = FastAPI()
 # Add CORS middleware configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Add your React frontend URL
+    allow_origins=["https://drona-three.vercel.app/"],  
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
