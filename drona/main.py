46e21f86d993bcb28cf40ed8ae38c727a9f0784b
Arman176001 <armanchaudhary176001@gmail.com>
2025-06-10 00:30:36 +0530

    Update main.py

--- drona/main.py
+++ drona/main.py
@@ -38,7 +38,7 @@ app = FastAPI()
 # Add CORS middleware configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://drona-three.vercel.app/"],
+    allow_origins=["https://drona-three.vercel.app"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"], 
